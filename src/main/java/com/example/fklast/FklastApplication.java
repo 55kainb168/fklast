4408a219a4414a4e0a89f665266425bf1325ed4b
lubenwei <215368433@qq.com>
2022-09-21 08:44:07 +0800

    初稿  2022年9月21日08:43:45

---
+++ src/main/java/com/example/fklast/FklastApplication.java
@@ -0,0 +1,15 @@
+package com.example.fklast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FklastApplication
+{
+
+    public static void main ( String[] args )
+    {
+        SpringApplication.run(FklastApplication.class, args);
+    }
+
+}
